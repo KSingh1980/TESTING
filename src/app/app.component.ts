87d1b30e4f98583c6259ecc11090456d09d65d15
Singh <Kulveer.Singh@amtrak.com>
2017-05-09 13:18:40 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component , OnInit, Inject, Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+ domain="";
+  constructor(@Inject(DOCUMENT) private document: any) { }
+
+    ngOnInit() {
+        this.domain = this.document.location.hostname;
+        console.log(this.domain);
+    }
 }
